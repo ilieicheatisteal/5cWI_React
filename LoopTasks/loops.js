411fc0461b228cc676973a4e2e744e8c28637cf1
ilieicheatisteal <eyupy99@gmail.com>
2025-03-15 23:02:08 +0100

    Loop 2

--- LoopTasks/loops.js
+++ LoopTasks/loops.js
@@ -2,3 +2,10 @@
 for (let i = 1; i <= 50; i++) {
     if (i % 2 === 0) console.log(i);
   }
+
+//sum ungerade zahlen
+let sum = 0;
+for (let i = 1; i <= 100; i += 2) {
+  sum += i;
+}
+console.log("Summe:", sum);
