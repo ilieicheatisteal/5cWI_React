ae4b1029110a1f8a1f0761cb3875505a25ba138c
ilieicheatisteal <eyupy99@gmail.com>
2025-03-15 23:03:34 +0100

    Loop 3

--- LoopTasks/loops.js
+++ LoopTasks/loops.js
@@ -9,3 +9,12 @@ for (let i = 1; i <= 100; i += 2) {
   sum += i;
 }
 console.log("Summe:", sum);
+
+//fizzbuzz
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+  }
+
