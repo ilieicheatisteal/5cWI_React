b994f7b9522d2ff0f07e15bc36f5655514dfe7e4
ilieicheatisteal <eyupy99@gmail.com>
2025-03-16 21:58:03 +0100

    Loop 9

--- LoopTasks/loops.js
+++ LoopTasks/loops.js
@@ -68,3 +68,12 @@ for (let i = 1; i <= 50; i++) {
   if (i % 7 === 0) count2++;
 }
 console.log(count2);
+
+//boom
+for (let i = 1; i <= 100; i++) {
+  if (i % 10 === 0) {
+    console.log("Boom");
+  } else {
+    console.log(i);
+  }
+}
